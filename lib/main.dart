f9bfd7a3a0f5d4647cb796142cd84cedf2ec7fce
Skyglar <olegs.logos@gmail.com>
2020-02-25 12:57:26 +0200

    color transition

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -7,16 +6,13 @@ void main() => runApp(MaterialApp(
 ));
 
 class Home extends StatefulWidget {
-
   @override
   _HomeState createState() => _HomeState();
-
 }
 
 class _HomeState extends State<Home> {
   Random random = new Random();
-
-  Color _color = Colors.black;
+  Color _color = Colors.grey;
 
   @override
   Widget build(BuildContext context) {
@@ -32,13 +28,14 @@ class _HomeState extends State<Home> {
           print('clicked');
         },
         child: SizedBox.expand(
-          child: Container(
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 700),
             color: _color,
             child: Center(
               child: Text(
                 'HELLo THERE',
                 style: TextStyle(
-                  fontSize: 20.0,
+                  fontSize: 24.0,
                   fontWeight: FontWeight.bold,
                   fontFamily: 'Starjedi',
                   color: Colors.amber,
@@ -75,62 +72,7 @@ class _HomeState extends State<Home> {
     String chars = '0123456789ABCDEF';
     String hex = '0x';
     while(length-- > 0) hex += chars[(random.nextInt(16)) | 0];
+
     return hex;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//class Home extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    Color _color = Colors.amber;
-//
-//    return Scaffold(
-//      body: GestureDetector(
-//        onTap: () {
-//          _color = Colors.deepPurple;
-//          print('clicked');
-//        },
-//        child: SizedBox.expand(
-//          child: Container(
-//            color: _color,
-//            child: Center(
-//              child: Text(
-//                'HELLo THERE',
-//                style: TextStyle(
-//                    fontSize: 20.0,
-//                    fontWeight: FontWeight.bold,
-//                    fontFamily: 'Starjedi'
-//                ),
-//              ),
-//            ),
-//          ),
-//        ),
-//      ),
-//    );
-//  }
-//}
-
-
-//Text(
-//'Hello there!',
-//style: TextStyle(
-//fontSize: 20.0,
-//fontWeight: FontWeight.bold,
-//letterSpacing: 2.0,
-//color: Colors.amberAccent,
-//fontFamily: 'Starjedi',
-//),
-//)
